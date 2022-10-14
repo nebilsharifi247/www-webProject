0053d2e034a9b378493b29cc71391007617a1139
Nebil sharifi <56500430+nebilsharifi247@users.noreply.github.com>
2022-10-14 19:26:10 +0330

    Update db.php
    Change username and password

--- db.php
+++ db.php
@@ -6,11 +6,12 @@ if(!isset($_SESSION['username'])){
   ob_start();
         header('Location: /login?notlogin=true');
 }
+This test username and password  you can set username  password  for you mysql sever .
 */
 $host="127.0.0.1";
-$user="root";
-$password="root";
-$database="post";
+$user="sample";
+$password="sample";
+$database="test_db";
 $connection=mysqli_connect($host,$user,$password,$database);
   $path="./imgs/";
       
